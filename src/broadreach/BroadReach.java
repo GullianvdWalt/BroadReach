2fbbdd1daa22e868678939f52e3bb27249f82f70
GullianvdWalt <51962834+GullianvdWalt@users.noreply.github.com>
2020-05-21 09:12:59 +0200

    Project Created. Dashboard View Created Clinician Views Created

---
+++ src/broadreach/BroadReach.java
@@ -0,0 +1,25 @@
+
+/**
+ *
+ * @author Gullian Van Der Walt H5G8YT7X3
+ * ITDA301 - Project 2020
+ * Pearson Pretoria 
+ * BSC IT Level 3
+ * 
+ * 
+ * 
+ * This Is The Main Class 
+ */
+package broadreach;
+
+public class BroadReach {
+
+
+
+    public static void main(String[] args) {
+        new Dashboard().setVisible(true);
+
+ 
+    }
+    
+}
